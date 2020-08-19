5bc28639f44b47df529e2e31cf17f321acbee72b
Daniel Maturana <dimatura@cmu.edu>
2020-08-19 03:21:14 -0400

    quick hack for ascii printability

--- rash/search.py
+++ rash/search.py
@@ -45,6 +45,11 @@ def search_run(output, **kwds):
 
     db = DataBase(cfstore.db_path)
     for crec in db.search_command_record(**preprocess_kwds(kwds)):
+        # output.write(format.format(**crec.__dict__))
+        # quick hack for ascii printability
+        for k, v in crec.__dict__.items():
+            if isinstance(v, unicode):
+                crec.__dict__[k] = v.encode('ascii', errors='replace')
         output.write(format.format(**crec.__dict__))
 
 
