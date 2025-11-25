71606903e157ca25f64ee81c808bb12d70fc6be5
Daniel Maturana <daniel.maturana@gather.ai>
2025-11-25 10:54:55 -0500

    attribution

--- rash/export.py
+++ rash/export.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2013-  Takafumi Arakaki
+# Copyright (C) 2013-  Takafumi Arakaki, Daniel Maturana+Opus4.5
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
